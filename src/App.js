d6a12932876ad197b1dcb7a0e496b30446ae9a29
Egor1o <egorklo147@gmail.com>
2024-12-06 15:03:09 +0200

    tailwind

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-violet-500">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-amber-400">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
